d37d72c6af18f22922ca47e1304bd01086994648
Felipe Bezerra <lipeilustra@gmail.com>
2026-03-28 23:45:44 -0300

    fix: backend deploy - porta dinâmica no Render e Swagger acessível

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ console.log("ENV TESTE:", process.env.DATABASE_URL); // 👈 IMPORTANTE
 
 import app from "./src/app.js";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+
